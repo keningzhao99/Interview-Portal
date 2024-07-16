494221a53417f2e5525359114ab88ff72edd43fe
Jennifer Zhuang <118019266+jzhuang03@users.noreply.github.com>
2024-07-16 15:29:35 -0400

    created react router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { Login } from "./routes/Login.jsx";
+import { Home } from "./routes/Home.jsx";
+import { UserProfile } from "./routes/UserProfile.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/profile",
+    element: <UserProfile />,
+  },
+  {
+    /* Add more if necessary for Forum page*/
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
