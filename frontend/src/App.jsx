4ce396972c5ee2aa658d36de45352d6dfa1cd487
Jennifer Zhuang <118019266+jzhuang03@users.noreply.github.com>
2024-07-20 23:12:55 -0400

    switching between admin and student login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <ChakraProvider>
       <h1> Launch Interview Portal </h1>
-    </>
+    </ChakraProvider>
   );
 }
 
--- frontend/src/routes/Login.jsx
+++ frontend/src/routes/Login.jsx
@@ -1,10 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Center,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Link,
+  Stack,
+  VStack,
+  Image,
+  FormHelperText,
+  Checkbox,
+} from "@chakra-ui/react";
 import "../styles/Login.css";
+import logo from "../assets/forge-stacked-primary.png";
 
 export const Login = () => {
+  // States for switching between student and admin login
+  const [adminLogin, setAdminLogin] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const toggleLoginType = () => {
+    setAdminLogin(!adminLogin);
+  };
+
   return (
-    <div>
-      <h1> Login </h1>
-    </div>
+    <Box className="login-container">
+      <Stack spacing="4">
+        <VStack as="header" spacing="6" mt="8">
+          <Image src={logo} alt="Forge Logo" className="logo" />
+          <Heading as="h1" className="heading">
+            {adminLogin ? "Admin Login" : "Student Login"}
+          </Heading>
+        </VStack>
+        <form onSubmit={handleSubmit}>
+          {adminLogin ? (
+            <>
+              <FormControl>
+                <FormLabel>Email</FormLabel>
+                <Input type="email" placeholder="Enter your email" />
+              </FormControl>
+              <FormControl mt={4}>
+                <FormLabel>Password</FormLabel>
+                <Input type="password" placeholder="Enter your password" />
+              </FormControl>
+            </>
+          ) : (
+            <>
+              <FormControl>
+                <FormLabel>Full Name</FormLabel>
+                <Input type="text" placeholder="Enter your full name" />
+              </FormControl>
+              <FormControl mt={4}>
+                <FormLabel>Phone Number</FormLabel>
+                <Input type="tel" placeholder="Enter your phone number" />
+              </FormControl>
+            </>
+          )}
+          <Stack mt={4}>
+            <Checkbox colorScheme="red" defaultChecked>
+              Remember Me
+            </Checkbox>
+          </Stack>
+          <Button mt={4} type="submit" className="sign-in-button">
+            LOGIN
+          </Button>
+          <Box mt={4}>
+            <Link href="#" className="toggle-link" onClick={toggleLoginType}>
+              {adminLogin ? "Student Login" : "Admin Login"}
+            </Link>
+          </Box>
+        </form>
+      </Stack>
+      <Center as="footer" mt={8}>
+        <FormControl>
+          <FormHelperText className="footer-text form-helper-text">
+            Copyright ©{" "}
+            <Link href="https://joinforge.co/" color="inherit" isExternal>
+              Forge
+            </Link>{" "}
+            2024.
+          </FormHelperText>
+        </FormControl>
+      </Center>
+    </Box>
   );
 };
