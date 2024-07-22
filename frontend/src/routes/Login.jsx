8bd1f18ff9c63c41bb423b110ec4b7ec93559002
Jennifer Zhuang <118019266+jzhuang03@users.noreply.github.com>
2024-07-22 03:00:46 -0400

    improving styling

--- frontend/src/routes/Login.jsx
+++ frontend/src/routes/Login.jsx
@@ -18,8 +18,8 @@ import "../styles/Login.css";
 import logo from "../assets/forge-stacked-primary.png";
 
 export const Login = () => {
-  // States for switching between student and admin login
   const [adminLogin, setAdminLogin] = useState(false);
+  const [rememberMe, setRememberMe] = useState(true);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -29,8 +29,14 @@ export const Login = () => {
     setAdminLogin(!adminLogin);
   };
 
+  const handleRememberMeChange = (e) => {
+    const isChecked = e.target.checked;
+    setRememberMe(isChecked);
+    // localStorage.setItem("memberMe", isChecked.toString());
+  };
+
   return (
-    <Box className="login-container">
+    <Box className="login-container" p={8} boxShadow="md" bg="gray.50">
       <Stack spacing="4">
         <VStack as="header" spacing="6" mt="8">
           <Image src={logo} alt="Forge Logo" className="logo" />
@@ -41,35 +47,57 @@ export const Login = () => {
         <form onSubmit={handleSubmit}>
           {adminLogin ? (
             <>
-              <FormControl>
-                <FormLabel>Email</FormLabel>
-                <Input type="email" placeholder="Enter your email" />
+              <FormControl className="login-form">
+                <FormLabel className="login-label">Email</FormLabel>
+                <Input
+                  className="login-input"
+                  type="email"
+                  placeholder="Enter your email"
+                />
               </FormControl>
-              <FormControl mt={4}>
-                <FormLabel>Password</FormLabel>
-                <Input type="password" placeholder="Enter your password" />
+              <FormControl mt={4} className="login-form">
+                <FormLabel className="login-label">Password</FormLabel>
+                <Input
+                  className="login-input"
+                  type="password"
+                  placeholder="Enter your password"
+                />
               </FormControl>
             </>
           ) : (
             <>
-              <FormControl>
-                <FormLabel>Full Name</FormLabel>
-                <Input type="text" placeholder="Enter your full name" />
+              <FormControl className="login-form">
+                <FormLabel className="login-label">Full Name</FormLabel>
+                <Input
+                  className="login-input"
+                  type="name"
+                  placeholder="Enter your full name"
+                />
               </FormControl>
-              <FormControl mt={4}>
-                <FormLabel>Phone Number</FormLabel>
-                <Input type="tel" placeholder="Enter your phone number" />
+              <FormControl mt={4} className="login-form">
+                <FormLabel className="login-label">Phone Number</FormLabel>
+                <Input
+                  className="login-input"
+                  type="telephone"
+                  placeholder="Enter your phone number"
+                />
               </FormControl>
             </>
           )}
           <Stack mt={4}>
-            <Checkbox colorScheme="red" defaultChecked>
+            <Checkbox
+              className="remember-me"
+              colorScheme="red"
+              isChecked={rememberMe}
+              onChange={handleRememberMeChange}
+            >
               Remember Me
             </Checkbox>
+
+            <Button mt={4} type="submit" className="sign-in-button">
+              LOGIN
+            </Button>
           </Stack>
-          <Button mt={4} type="submit" className="sign-in-button">
-            LOGIN
-          </Button>
           <Box mt={4}>
             <Link href="#" className="toggle-link" onClick={toggleLoginType}>
               {adminLogin ? "Student Login" : "Admin Login"}
