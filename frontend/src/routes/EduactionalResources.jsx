4ebcde88512f2f89df543d57bc907f6fafaca952
Omar <hmj9gf@Virginia.edu>
2024-08-02 17:00:05 -0400

    Updated resources page

--- frontend/src/routes/EduactionalResources.jsx
+++ frontend/src/routes/EduactionalResources.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Heading, VStack, Button, Text, Flex, Progress } from '@chakra-ui/react';
+import { Box, Heading, VStack, Flex, Progress, Text } from '@chakra-ui/react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { useToast } from '@chakra-ui/react';
-import { lessons } from './lessonData';
+import { lessons } from '../components/lessonData';
 
 const getInitialProgress = () => {
   const savedProgress = localStorage.getItem('lessonProgress');
@@ -46,59 +46,58 @@ export const EducationalResources = () => {
   const isLessonCompleted = (lesson) => lesson && lesson.every(task => task);
 
   return (
-    <Box p={5}>
-      <Heading as="h1" size="xl" mb={5} textAlign="center" color="purple.600">
-        Launch Interview Portal
-      </Heading>
-      <Heading as="h2" size="lg" mb={5} textAlign="center">Interview Preparation</Heading>
-      <Flex direction="column" align="center" justify="center" w="100%">
-        <VStack spacing={4} w="80%">
-          {lessons.map((lesson, index) => {
-            const lessonTasks = completedLessons[index] || [];
-            const progress = calculateProgress(lessonTasks);
-            const isLocked = !completedLessons.slice(0, index).every(isLessonCompleted);
-            return (
-              <Box
-                key={lesson.id}
-                p={4}
-                borderWidth={1}
-                borderRadius="md"
-                backgroundColor={
-                  isLessonCompleted(lessonTasks)
-                    ? 'green.50'
-                    : isLocked
-                      ? 'gray.200'
-                      : 'white'
-                }
-                w="100%"
-                onClick={() => {
-                  if (!isLocked) {
-                    navigate(`/educational-resources/${lesson.id}`, {
-                      state: { completedTasks: lessonTasks },
-                    });
+    <Box>
+      <Box p={5}>
+        <Heading as="h2" size="xl" mb={5} textAlign="center">Interview Preparation</Heading>
+        <Flex direction="column" align="center" justify="center" w="100%">
+          <VStack spacing={4} w="80%">
+            {lessons.map((lesson, index) => {
+              const lessonTasks = completedLessons[index] || [];
+              const progress = calculateProgress(lessonTasks);
+              const isLocked = !completedLessons.slice(0, index).every(isLessonCompleted);
+              return (
+                <Box
+                  key={lesson.id}
+                  p={4}
+                  borderWidth={1}
+                  borderRadius="md"
+                  backgroundColor={
+                    isLessonCompleted(lessonTasks)
+                      ? 'green.50'
+                      : isLocked
+                        ? 'gray.200'
+                        : 'white'
                   }
-                }}
-                cursor={!isLocked ? 'pointer' : 'not-allowed'}
-              >
-                <Flex direction="row" justify="space-between" align="center">
-                  <VStack spacing={4} align="flex-start">
-                    <Heading as="h2" size="md">{lesson.title}</Heading>
-                    <Text>{lesson.description}</Text>
-                  </VStack>
-                  <Box width="20%">
-                    {progress > 0 && progress < 100 && (
-                      <Progress value={progress} size="sm" colorScheme="yellow" width="100%" />
-                    )}
-                    {progress === 100 && (
-                      <Text color="green.500" textAlign="right">Completed</Text>
-                    )}
-                  </Box>
-                </Flex>
-              </Box>
-            );
-          })}
-        </VStack>
-      </Flex>
+                  w="100%"
+                  onClick={() => {
+                    if (!isLocked) {
+                      navigate(`/educational-resources/${lesson.id}`, {
+                        state: { completedTasks: lessonTasks },
+                      });
+                    }
+                  }}
+                  cursor={!isLocked ? 'pointer' : 'not-allowed'}
+                >
+                  <Flex direction="row" justify="space-between" align="center">
+                    <VStack spacing={4} align="flex-start">
+                      <Heading as="h2" size="md">{lesson.title}</Heading>
+                      <Text>{lesson.description}</Text>
+                    </VStack>
+                    <Box width="20%">
+                      {progress > 0 && progress < 100 && (
+                        <Progress value={progress} size="sm" colorScheme="yellow" width="100%" />
+                      )}
+                      {progress === 100 && (
+                        <Text color="green.500" textAlign="right">Completed</Text>
+                      )}
+                    </Box>
+                  </Flex>
+                </Box>
+              );
+            })}
+          </VStack>
+        </Flex>
+      </Box>
     </Box>
   );
 };
